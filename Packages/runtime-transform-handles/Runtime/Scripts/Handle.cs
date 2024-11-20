c85f23fa40e935b2a27859af5051bac89da06aa6
B1naryB0b <35399675+B1naryB0b@users.noreply.github.com>
2024-11-20 20:21:19 +0000

    Fixed Namespaces

--- Packages/runtime-transform-handles/Runtime/Scripts/Handle.cs
+++ Packages/runtime-transform-handles/Runtime/Scripts/Handle.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TransformHandles.Utils;
 using UnityEngine;
 
 namespace TransformHandles
--- Packages/runtime-transform-handles/Runtime/Scripts/Utils/Singleton.cs
+++ Packages/runtime-transform-handles/Runtime/Scripts/Utils/Singleton.cs
@@ -1,62 +1,67 @@
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+namespace TransformHandles.Utils
 {
-    private static T _instance;
-
-    public static T Instance
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
     {
-        get
+        private static T _instance;
+
+        public static T Instance
         {
-            if (ApplicationQuitManager.ApplicationQuitting)
+            get
             {
-                return null;
+                if (ApplicationQuitManager.ApplicationQuitting)
+                {
+                    return null;
+                }
+
+                if (_instance != null) return _instance;
+                _instance = FindObjectOfType<T>();
+
+                if (_instance != null) return _instance;
+                var obj = new GameObject(typeof(T).Name);
+                _instance = obj.AddComponent<T>();
+
+                return _instance;
             }
+        }
 
-            if (_instance != null) return _instance;
-            _instance = FindObjectOfType<T>();
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = (T)this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
 
-            if (_instance != null) return _instance;
-            var obj = new GameObject(typeof(T).Name);
-            _instance = obj.AddComponent<T>();
+        protected virtual void OnDestroy()
+        {
+            if (_instance == this)
+            {
+                _instance = null;
+            }
+        }
 
-            return _instance;
+        private void OnApplicationQuit()
+        {
+            ApplicationQuitManager.SetApplicationQuitting(true);
         }
     }
-
-    protected virtual void Awake()
+    
+    public static class ApplicationQuitManager
     {
-        if (_instance == null)
+        public static bool ApplicationQuitting { get; private set; }
+    
+        public static void SetApplicationQuitting(bool quitting)
         {
-            _instance = (T)this;
-            DontDestroyOnLoad(gameObject);
+            ApplicationQuitting = quitting;
         }
-        else if (_instance != this)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    protected virtual void OnDestroy()
-    {
-        if (_instance == this)
-        {
-            _instance = null;
-        }
-    }
-
-    private void OnApplicationQuit()
-    {
-        ApplicationQuitManager.SetApplicationQuitting(true);
     }
 }
 
-public static class ApplicationQuitManager
-{
-    public static bool ApplicationQuitting { get; private set; }
 
-    public static void SetApplicationQuitting(bool quitting)
-    {
-        ApplicationQuitting = quitting;
-    }
-}
